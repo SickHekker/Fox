21b5a77bab3fe4dc078265f677e5de6f63a80bbf
SickHekker <Robindehek@gmail.com>
2017-01-27 08:05:49 +0100

    added some comments

--- fox.py
+++ fox.py
@@ -17,17 +17,24 @@ turn = 100
 try:
         while True:   
             char = screen.getch()
+	#pressing Q stops the robot and returns you to the commandline
             if char == ord('q'):
                 break
 		robot.stop()
+	#if you press W will keep driving forward until you press E to stop it
+	#note: you can also make it go another direction instead of pressing E to fully stop the tank
             elif char == ord('w'):
                 robot.forward(speed)
+	#if you press S will keep driving forward until you press E to stop it
             elif char == ord('s'):
                 robot.backward(speed)
+	#if you press D will keep rotating to the right until you press E to stop it
             elif char == ord('d'):
                 robot.right(turn)
+	#if you press A will keep turning left until you press E to stop it
             elif char == ord('a'):
                 robot.left(turn)
+	#if you press E you stop the tank
             elif char == ord('e'):
                 robot.stop()
  	    elif char == ord('1'):
